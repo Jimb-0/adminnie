33b430ad19333a7c9399464ef8be330e376bb18e
Jimmy <cjwillis92@yahoo.com>
2019-03-13 13:47:50 +0000

    added pics updated services

--- index/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from index.models import Login_Model, Quote, Insurance, Comment
-
-admin.site.register(Login_Model)
-admin.site.register(Quote)
-admin.site.register(Insurance)
-admin.site.register(Comment)
-
-# Register your models here.
